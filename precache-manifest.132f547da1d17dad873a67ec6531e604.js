79ba5ce28393b3122ee0f74e0bb7a242bd8a6329
Sergio Tellez <sergiotellezsanchez@gmail.com>
2020-04-17 12:58:59 -0500

    Updates

---
+++ precache-manifest.132f547da1d17dad873a67ec6531e604.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5504834ffabdb49e06e9549aaa9142",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1e69547ec3087a0f3a7f",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "d66c90b4f21f04f985ed",
+    "url": "/robofriends/static/css/main.dbe84dd6.chunk.css"
+  },
+  {
+    "revision": "1e69547ec3087a0f3a7f",
+    "url": "/robofriends/static/js/2.ebe8a8ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ebe8a8ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66c90b4f21f04f985ed",
+    "url": "/robofriends/static/js/main.998f368a.chunk.js"
+  },
+  {
+    "revision": "c731659b160a0cd87b12",
+    "url": "/robofriends/static/js/runtime-main.60024418.js"
+  },
+  {
+    "revision": "2c84a11cbaeabb9b2adebec946cbdb56",
+    "url": "/robofriends/static/media/Spongeboy Me Bob.2c84a11c.woff"
+  }
+]);
